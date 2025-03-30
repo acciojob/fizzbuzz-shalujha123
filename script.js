62f04f4568718af1019a4d4226069d559b72eb3b
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-30 21:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function fizzBuzz(){
+	let num = alert("Choose numbers from 1 to 20" )
+	if(num%3==0){
+		console.log("Fizz")
+	}else if(num%5==0){
+		console.log("Buzz")
+	}else if(num%3==0 && num%5==0){
+		console.log("FizzBuzz")
+	}else{
+		console.log(num)
+	}
+}
+fizzBuzz()
